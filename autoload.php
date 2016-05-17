dcee804692b6736050133870b55ad0fc7e9f1687
Rob Loach <robloach@gmail.com>
2016-05-17 04:01:54 -0400

    Update to Drupal 8.1.1

--- autoload.php
+++ autoload.php
@@ -1,15 +1,14 @@
 <?php
 
-$includeIfExists = function ()
-{
-    return file_exists($file) ? include $file : false;
-}
+/**
+ * @file
+ * Includes the autoloader created by Composer.
+ *
+ * @see composer.json
+ * @see index.php
+ * @see core/install.php
+ * @see core/rebuild.php
+ * @see core/modules/statistics/statistics.php
+ */
 
-if ((!$loader = $includeIfExists(__DIR__.'/../vendor/autoload.php')) && (!$loader = $includeIfExists(__DIR__.'/../../../autoload.php'))) {
-    echo 'You must set up the project dependencies, run the following commands:'.PHP_EOL.
-        'curl -sS https://getcomposer.org/installer | php'.PHP_EOL.
-        'php composer.phar install'.PHP_EOL;
-    exit(1);
-}
-
-return $loader;
+return require __DIR__ . '/vendor/autoload.php';
